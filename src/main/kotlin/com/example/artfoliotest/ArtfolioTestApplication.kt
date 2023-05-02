0a63527302a184373012b1ee943991facf8e6a33
seungsu <h970126@gmail.com>
2023-05-02 16:21:17 +0900

    Initialize Project

---
+++ src/main/kotlin/com/example/artfoliotest/ArtfolioTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.artfoliotest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArtfolioTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArtfolioTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/artfoliotest/ArtfolioTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.artfoliotest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArtfolioTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
